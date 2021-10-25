b39b7b88250bf29d421afb3c8187fecd13bdba6a
PaulinaAR <93107532+PaulinaAR@users.noreply.github.com>
2021-10-24 21:01:32 -0600

    Add files via upload

---
+++ EVA1_22_TIEMPO_FOR/src/eva1_22_tiempo_for/EVA1_22_TIEMPO_FOR.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_22_tiempo_for;
+
+/**
+ *
+ * @author asus X556U
+ */
+public class EVA1_22_TIEMPO_FOR {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      int[]aiArray = new int[100000000];//400 MEGAS
+      for (int i = 0; i < aiArray.length; i++) {
+         aiArray[i] = 100;
+         
+      }
+   }
+   
+}
